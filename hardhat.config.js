793cf8c7f94a46690c03e47c98fb5ff7675e2761
Linus Weigand <linusweigand@protonmail.com>
2022-04-13 17:16:45 +0200

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,8 +5,8 @@ module.export = {
   solidity: '0.8.0',
   networks: {
     rinkeby: {
-      url: "https://eth-rinkeby.alchemyapi.io/v2/1vQY62g6fT4VMUAv6x2psIVZvaGWIFrO",
-      accounts: ["0xddb21e4575a175509b890d4b65cfac4e5e031034c4925e0fb5428dee2b717bbe"],
+      url: process.env.ALCHEMY_API_URL,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
