6959c3cb9c604c92e2eba3bc32417a838752f958
Linus Weigand <linusweigand@protonmail.com>
2022-04-13 14:44:53 +0200

    Added wave list

--- src/App.js
+++ src/App.js
@@ -151,6 +151,16 @@ const App = () => {
             Connect Wallet
           </button>
         )}
+
+        {allWaves.map((wave, index) => {
+          return (
+            <div key={index} style={{ backgroundColor: "OldLace", marginTop: "16px", padding: "8px"}}>
+              <div>Address: {wave.address}</div>
+              <div>Time: {wave.timestamp.toString()}</div>
+              <div>Message: {wave.message}</div>  
+            </div>
+          )
+        })}
       </div>
     </div>
   );
